5a02b64ea1de8324dcfc7005c6dd53e6ff7f0776
Farhan Aldiansyah <revalinoaldi@gmail.com>
2023-03-08 09:52:19 +0700

    fixing sh files

--- assets/nfs-setup-server.sh
+++ assets/nfs-setup-server.sh
@@ -1,7 +1,7 @@
 dnf install nfs-utils && \
 systemctl start nfs-server.service && \
-systemctl start nfs-server.service && \
+systemctl enable nfs-server.service && \
 systemctl status nfs-server.service && \
-echo "/var/www/ifgl-frontend/frontend/web/uploads/ 10.10.1.150(rw,sync,no_all_squash,root_squash)">>/etc/exports && \
+echo "/var/www/ifgl-frontend/frontend/web/uploads/ 10.10.20.186(rw,sync,no_all_squash,root_squash)">>/etc/exports && \
 exportfs -arv && \
 exportfs  -s
