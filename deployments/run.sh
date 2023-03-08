423139147e94802d5fb73ae2cc004e3a07bcf8f3
Farhan Aldiansyah <revalinoaldi@gmail.com>
2023-03-08 09:53:16 +0700

    Fixing sh files

--- deployments/run.sh
+++ deployments/run.sh
@@ -3,6 +3,6 @@ sh scripts/runserver.sh && \
 mkdir -p -m 0777 /var/www/ifgl_frontend/frontend/web/uploads/ && \
 chmod -R 777 /var/www/ifgl_frontend && \
 dnf install nfs-utils nfs4-acl-tools && \
-showmount -e 10.10.1.131 && \
-mount -t nfs 10.10.1.131:/var/www/ifgl_frontend/frontend/web/uploads/ /var/www/ifgl_frontend/frontend/web/uploads/ && \
-echo "10.10.1.131:/var/www/ifgl-frontend/frontend/web/uploads/ /var/www/ifgl-frontend/frontend/web/uploads/ nfs defaults 0 0">>/etc/fstab
+showmount -e 10.10.20.200 && \
+mount -t nfs 10.10.20.200:/var/www/ifgl_frontend/frontend/web/uploads/ /var/www/ifgl_frontend/frontend/web/uploads/ && \
+echo "10.10.20.200:/var/www/ifgl-frontend/frontend/web/uploads/ /var/www/ifgl-frontend/frontend/web/uploads/ nfs defaults 0 0">>/etc/fstab
